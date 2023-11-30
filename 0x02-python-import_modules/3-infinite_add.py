e6afa7e71d22e0ea5920439ed0d4db181fece01a
margret739 <margretandalya@gmail.com>
2023-11-30 06:53:16 -0800

    infinit adds

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    count = 0
+    import sys, math
+    result = 0
     for i in range(len(sys.argv) - 1):
-        count += int(sys.argv[i + 1])
-        print("{}".format(count))
+        result += int(sys.argv[i + 1])
+        print("{}".format(result))
