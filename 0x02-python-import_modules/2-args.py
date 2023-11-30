c28abd9af7931bcdd3c7793f9c7264c8803cd44b
margret739 <margretandalya@gmail.com>
2023-11-30 06:03:00 -0800

    argument

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    arg = len(sys.argv)
+    if arg == 1:
+        print("{} arguments.".format(arg - 1))
+    elif arg == 2:
+        print("{} argument:".format(arg - 1))
+    else:
+        print("{} arguments:".format(arg - 1))
+        for j in range(1, arg):
+            print("{}:{}".format(j, sys.argv[j]))
