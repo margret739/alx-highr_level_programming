c73282365cca7d2641c7b10698e5682e633ea087
margret739 <margretandalya@gmail.com>
2023-12-08 06:03:58 -0800

    print sort

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,7 +2,6 @@
 def print_sorted_dictionary(a_dictionary):
     sort_list = list(a_dictionary.keys())
     sort_list.sort()
-
     for i in sort_list:
         print("{}: {}".format(i, a_dictionary.get(i)))
 
