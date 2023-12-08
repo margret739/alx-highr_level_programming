b8b8add2cb69ed10f2f1714ac23b2f8f54df7e3b
margret739 <margretandalya@gmail.com>
2023-12-08 06:42:23 -0800

    weight average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    number = 0
+    d = 0
+    for put in my_list:
+        number += put[0] * put[1]
+        d += put[1]
+    return (num / den)
