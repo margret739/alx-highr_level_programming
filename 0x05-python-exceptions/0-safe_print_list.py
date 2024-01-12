6938779a2a9245bac735585430850e8d767a950d
margret739 <margretandalya@gmail.com>
2024-01-11 20:34:54 -0800

    tasks

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     num = 0
     for i in range(x):
@@ -8,4 +8,4 @@ def safe_print_list(my_list=[], x=0):
         except IndexError:
             break
         print("")
-        Return
+        return
---
+++ README.md
@@ -0,0 +1,1 @@
+Alx_level_programming
